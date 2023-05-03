5999c4908ae2d0b6eb3079cda4c296df9258cf96
Claudio <claudio1419teixeira2020@gmail.com>
2023-05-03 20:49:11 -0300

    init

---
+++ courses/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('<int:pk>/', views.details, name='details'),
+    path('<slug:slug>/', views.details, name='details')
+
+]
