cfb77a8aa7a8e45df4019686fbc1e8863abcc3af
Claudio <claudio1419teixeira2020@gmail.com>
2023-05-05 10:58:55 -0300

    create login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
 urlpatterns = [
     path('entrar', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('cadastre-se', views.register, name='register')
+    path('cadastre-se', views.register, name='register'),
+    path('sair', LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
