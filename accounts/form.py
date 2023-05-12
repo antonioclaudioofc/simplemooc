c1b8189642ac8a652d23fd47601cac7d19ca82db
Claudio <claudio1419teixeira2020@gmail.com>
2023-05-12 17:45:50 -0300

    create reset password

--- accounts/form.py
+++ accounts/form.py
@@ -1,8 +1,31 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+
 from django.contrib.auth import get_user_model
+from accounts.models import PasswordReset
+
+from core.utils import generate_hash_key
 
 User = get_user_model()
+
+
+class PasswordResetForm(forms.Form):
+    email = forms.EmailField(label='E-mail')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+
+        if User.objects.filter(email=email).exists():
+            return email
+        raise forms.ValidationError('Nenhum usuário encontrado com este e-mail')
+    
+    def save(self):
+        user = User.objects.get(email=form.cleaned_data['email'])
+        key = generate_hash_key(user.username)
+        reset = PasswordReset(key=key, user=user)
+        reset.save()
+        template_name = 'accounts/password_reset.html'
+
 
 
 class RegisterForm(forms.ModelForm):
