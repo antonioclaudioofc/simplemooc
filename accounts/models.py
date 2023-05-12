12ed39c5f733e756e9f7961836b71f76f445c8a3
Claudio <claudio1419teixeira2020@gmail.com>
2023-05-11 22:00:36 -0300

    create new db.sqlite3 and useraccounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,19 @@
+import re
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.core import validators
+from django.conf import settings
 # Create your models here.
+
 
 class User(AbstractBaseUser, PermissionsMixin):
 
-    username = models.CharField('Nome de Usuário', max_length=30, unique=True)
+    username = models.CharField('Nome de Usuário', max_length=30, unique=True, validators=[validators.RegexValidator(re.compile(
+        '^[\w.@+-]+$'), 'O nome de usuário só pode conter letras. digitos ou os seguintes caracteres: @/./+/-/_', 'invalid')])
     email = models.EmailField('E-mail', unique=True)
     name = models.CharField('Nome', max_length=100, blank=True)
-    is_active = models.BooleanField('Está ativo?', blank=True, default=False)
+    is_active = models.BooleanField('Está ativo?', blank=True, default=True)
     is_staff = models.BooleanField('É da equipe?', blank=True, default=False)
     date_joined = models.DateTimeField('Data de Entrada', auto_now_add=True)
 
@@ -18,13 +24,29 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self) -> str:
         return self.name or self.username
-    
+
     def get_short_name(self):
         return self.username
 
     def get_full_name(self):
         return str(self)
-    
+
     class Meta:
         verbose_name = 'Usuário'
         verbose_name_plural = 'Usuários'
+
+
+class PasswordReset(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Usuário')
+    key = models.CharField('Chave', max_length=100, unique=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+
+    def __str__(self) -> str:
+        return '{0} em {1}'.format(self.user, self.created_at)
+    confirm = models.BooleanField('Confirmado?', default=False, blank=True)
+    
+    class Meta:
+        verbose_name = 'Nova Senha'
+        verbose_name_plural = 'Novas Senhas'
+        ordering = ['-created_at']
