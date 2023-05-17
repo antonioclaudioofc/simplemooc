38d9e8d62f1e326040e4a03aacfa0e85ae762f85
Claudio <claudio1419teixeira2020@gmail.com>
2023-05-17 10:27:04 -0300

    create announcements

--- courses/urls.py
+++ courses/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # path('<int:pk>/', views.details, name='details'),
     path('<slug:slug>/', views.details, name='details'),
-    path('<slug:slug>/inscricao', views.enrollment, name='enrollment')
-
+    path('<slug:slug>/inscricao', views.enrollment, name='enrollment'),
+    path('<slug:slug>/anuncios', views.announcements, name='announcements'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .models import Course, Enrollment
 from .forms import ContactCourse
 
 # Create your views here.
+
+
 def index(request):
     courses = Course.objects.all()
     template_name = 'courses/index.html'
@@ -45,15 +47,36 @@ def details(request, slug):
 
     return render(request, template_name, context)
 
+
 @login_required
 def enrollment(request, slug):
     course = get_object_or_404(Course, slug=slug)
-    enrollment, created = Enrollment.objects.get_or_create(user=request.user, course=course)
+    enrollment, created = Enrollment.objects.get_or_create(
+        user=request.user, course=course)
 
     if created:
         enrollment.active()
         messages.success(request, 'Você foi inscriro no curso com sucesso')
     else:
         messages.info(request, 'Você já está inscrito no curso')
-    
+
     return redirect('accounts:dashboard')
+
+
+@login_required
+def announcements(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+
+    if not request.user.is_staff:
+
+        enrollment = get_object_or_404(
+            Enrollment, user=request.user, course=course
+        )
+
+        if not enrollment.is_approved():
+            messages.error(request, 'Inscrição pendente !')
+        return redirect('accounts:dashboard')
+    
+    template_name = 'courses/announcements.html'
+    context = {'course': course}
+    return render(request, template_name, context)
